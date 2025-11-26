d1ead992523eaa78ffdbbb638765a63a0a2921bd
fernandoalmeida999 <fernandoalmeida110607@gmail.com>
2025-11-26 22:09:49 +0000

    app.jsx

--- app.jsx
+++ app.jsx
@@ -1,1 +1,23 @@
-import routes from './routes';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import home from "./pages/home";
+import studentspage from "./pages/studentspage";
+import groupspage from "./pages/groupspage";
+import notfound from "./pages/notfound";
+import navbar from "./components/navbar";
+
+function app() {
+  return (
+    <Router>
+      <navbar />
+
+      <Routes>
+        <Route path="/" element={<home />} />
+        <Route path="/students" element={<studentspage />} />
+        <Route path="/groups" element={<groupspage />} />
+        <Route path="*" element={<notfound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default app;
